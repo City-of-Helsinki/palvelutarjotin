507a3ff4323a38f4ae9be326732f5140624da67f
Khin Loc <khin.loc@anders.fi>
2020-09-28 13:06:25 +0300

    Update django admin interface

--- occurrences/admin.py
+++ occurrences/admin.py
@@ -9,32 +9,62 @@ from occurrences.models import (
 from parler.admin import TranslatableAdmin
 
 
+class OccurrenceInline(admin.TabularInline):
+    model = Occurrence.study_groups.through
+
+
 @admin.register(StudyGroup)
 class StudyGroupAdmin(admin.ModelAdmin):
     list_display = ("name", "created_at", "group_size")
     exclude = ("id",)
+    inlines = (OccurrenceInline,)
 
 
 @admin.register(Occurrence)
 class OccurrenceAdmin(admin.ModelAdmin):
     list_display = (
-        "place_id",
-        "created_at",
+        "p_event",
+        "auto_acceptance",
+        "start_time",
+        "end_time",
+        "amount_of_seats",
+        "seats_taken",
+        "cancelled",
         "updated_at",
     )
     exclude = ("id",)
 
+    def linked_event_id(self, obj):
+        return obj.p_event.linked_event_id
+
 
 @admin.register(Enrolment)
 class EnrolmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "study_group", "linked_event_id", "status")
+    list_display_links = ("id", "study_group")
     readonly_fields = ("enrolment_time",)
+
+    def linked_event_id(self, obj):
+        return obj.occurrence.p_event.linked_event_id
 
 
 @admin.register(VenueCustomData)
 class VenueCustomDataAdmin(TranslatableAdmin):
-    list_display = ("place_id",)
+    list_display = ("place_id", "has_clothing_storage", "has_snack_eating_place")
 
 
 @admin.register(PalvelutarjotinEvent)
 class PalvelutarjotinEventAdmin(admin.ModelAdmin):
-    list_display = ("linked_event_id", "enrolment_start", "enrolment_end_days")
+    list_display = (
+        "linked_event_id",
+        "organisation",
+        "occurrences_count",
+        "enrolment_start",
+        "duration",
+        "enrolment_end_days",
+        "needed_occurrences",
+        "contact_email",
+    )
+
+    def occurrences_count(self, obj):
+        return obj.occurrences.count()
