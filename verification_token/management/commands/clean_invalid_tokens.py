eb92e50f1f01c01d53ad31d2acb07acbdbdbabfa
Niko Lindroos <niko.j.makela@gmail.com>
2021-01-21 11:50:14 +0200

    Added a management function and a cron job to clean invalid tokens.
    PT-711.

---
+++ verification_token/management/commands/clean_invalid_tokens.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from verification_token.models import VerificationToken
+
+
+class Command(BaseCommand):
+    help = "Clean inactive and expired verification tokens."
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            "--inactive",
+            action="store_true",
+            help="Delete only verification tokens which are inactive "
+            + "(is_active = False)",
+        )
+        parser.add_argument(
+            "--expired",
+            action="store_true",
+            help="Delete only verification tokens which are expired "
+            + "(expiry_date_lte = timezone.now())",
+        )
+
+    def handle(self, *args, **options):
+        if options["inactive"] or options["expired"]:
+            VerificationToken.objects.clean_invalid_tokens(
+                clean_inactive=options["inactive"], clean_expired=options["expired"]
+            )
+        else:
+            VerificationToken.objects.clean_invalid_tokens()
