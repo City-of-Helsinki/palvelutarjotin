837180f7b0b02c889e6a36e22b71864576256641
Niko Lindroos <niko.j.makela@gmail.com>
2021-11-30 09:19:50 +0200

    Added a Size2ArrayFieldBase serializer for "tuple fields"
    PT-1369

--- reports/serializers.py
+++ reports/serializers.py
@@ -1,8 +1,42 @@
+from typing import List
+
 from reports.models import EnrolmentReport
 from rest_framework import serializers
 
 
+class Size2ArrayField(serializers.Field):
+    def __init__(self, first_value_name: str = "id", second_value_name: str = "label"):
+        self.first_value_name = first_value_name
+        self.second_value_name = second_value_name
+        super().__init__()
+
+    def _array_to_dict(self, entry: List[str]):
+        if len(entry) != 2:
+            raise ValueError(
+                "Size2ArrayField field value should always "
+                "have a length of 2 (id, interpretation)."
+            )
+        return {self.first_value_name: entry[0], self.second_value_name: entry[1]}
+
+    def _dict_to_array(self, entry: dict):
+        return [entry[self.first_value_name], entry[self.second_value_name]]
+
+    def to_representation(self, value):
+        return [self._array_to_dict(entry) for entry in value] if value else None
+
+    def to_internal_value(self, data):
+        return [self._dict_to_array(entry) for entry in data] if data else None
+
+
 class EnrolmentReportSerializer(serializers.ModelSerializer):
+    study_group_study_levels = Size2ArrayField(
+        first_value_name="id", second_value_name="label"
+    )
+    occurrence_languages = Size2ArrayField(
+        first_value_name="id", second_value_name="name"
+    )
+    keywords = Size2ArrayField(first_value_name="id", second_value_name="name")
+
     class Meta:
         model = EnrolmentReport
         fields = "__all__"
---
+++ reports/tests/test_serializers.py
@@ -0,0 +1,43 @@
+from reports.models import EnrolmentReport
+from reports.serializers import EnrolmentReportSerializer
+
+
+def test_enrolment_report_to_representation():
+    report = EnrolmentReport(
+        occurrence_languages=[["fi", "Finnish"]],
+        study_group_study_levels=[["age_0_2", "age 0-2"]],
+        keywords=[["kultus:5", "Musiikki"]],
+    )
+    serializer = EnrolmentReportSerializer(instance=report)
+    assert serializer.data["study_group_study_levels"][0] == {
+        "id": "age_0_2",
+        "label": "age 0-2",
+    }
+    assert serializer.data["occurrence_languages"][0] == {"id": "fi", "name": "Finnish"}
+    assert serializer.data["keywords"][0] == {"id": "kultus:5", "name": "Musiikki"}
+
+
+def test_enrolment_report_to_internal():
+    report_json = {
+        "study_group_study_levels": [{"id": "age_0_2", "label": "age 0-2"}],
+        "occurrence_languages": [{"id": "fi", "name": "Finnish"}],
+        "keywords": [{"id": "kultus:5", "name": "Musiikki"}],
+        # Rest are for is_valid -call
+        "study_group_amount_of_children": 1,
+        "study_group_amount_of_adult": 1,
+        "enrolment_time": "2021-11-08T17:16:55.356724+02:00",
+        "enrolment_status": "approved",
+        "occurrence_place_id": "tprek:59265",
+        "occurrence_cancelled": False,
+        "occurrence_amount_of_seats": 10,
+        "occurrence_start_time": "2021-11-14T00:00:00+02:00",
+        "occurrence_end_time": "2021-11-14T01:00:00+02:00",
+        "linked_event_id": "local-kultus:af6p5lyfxu",
+        "enrolment_start_time": "2021-11-08T10:45:38.439000+02:00",
+    }
+    report = EnrolmentReportSerializer(data=report_json)
+    report.is_valid()
+    data = report.validated_data
+    assert data["occurrence_languages"][0] == ["fi", "Finnish"]
+    assert data["study_group_study_levels"][0] == ["age_0_2", "age 0-2"]
+    assert data["keywords"][0] == ["kultus:5", "Musiikki"]
