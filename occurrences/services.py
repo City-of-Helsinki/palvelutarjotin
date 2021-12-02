646a2375357f9cf5232c1d82bffcb9ac716d600c
Niko Lindroos <niko.j.makela@gmail.com>
2021-12-02 12:02:17 +0200

    Refactored send_enrolment_summary_report_to_providers
    Enrolment used EnrolmentQuerySet as a manager, which lead to a situation where notification sending logic was part of the query set method. A query set should not have a business logic inside it, but only return a query set, so created a new EnrolmentManager to manage the notification sending. However, its not even managers job to maintain the logic for notification sending, so moved the sending logic to a new service module.
    
    PT-1364.

---
+++ occurrences/services.py
@@ -0,0 +1,46 @@
+from typing import List, Union
+
+import occurrences.models as occurrences_models
+from django.db import models
+from django_ilmoitin.utils import send_notification
+from occurrences.consts import NotificationTemplate
+
+
+def send_enrolment_summary_report_to_providers(
+    enrolments: Union[models.QuerySet, List["occurrences_models.Enrolment"]]
+):
+    reports = {}
+    p_events = (
+        occurrences_models.PalvelutarjotinEvent.objects.filter(
+            occurrences__enrolments__in=enrolments
+        )
+        .prefetch_related("occurrences__enrolments")
+        .distinct()
+    )
+
+    for p_event in p_events:
+        # Group by contact_email address:
+        reports.setdefault(p_event.contact_email, []).append(p_event)
+
+    for address, report in reports.items():
+        context_report = []
+        for p_event in report:
+            context_report.append(
+                {
+                    "event": p_event.get_event_data(),
+                    "p_event": p_event,
+                    "occurrences": p_event.occurrences.filter(
+                        enrolments__in=enrolments
+                    ).distinct(),
+                }
+            )
+        context = {
+            "report": context_report,
+            "total_pending_enrolments": enrolments.count_pending_enrolments_by_email(
+                address
+            ),
+            "total_new_enrolments": enrolments.count_new_enrolments_by_email(address),
+        }
+        send_notification(
+            address, NotificationTemplate.ENROLMENT_SUMMARY_REPORT, context
+        )
