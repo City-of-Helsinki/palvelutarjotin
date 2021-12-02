987e41da78651e2ea98ff0d826e11c87b3627c1d
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-12-03 00:12:42 +0200

    Add function to calculate bounding box for coordinates
    LinkedEvents provides a bounding box filter events. The
    distance that the added function uses is from the given
    coordinates to the corner of the bounding box.
    
    Refs PT-1289

--- graphene_linked_events/utils.py
+++ graphene_linked_events/utils.py
@@ -2,8 +2,11 @@
 # -into-a-python-object/15882054#15882054
 import json
 from collections import namedtuple
+from typing import List
 
 from django.conf import settings
+from geopy import distance as geopy_distance
+from geopy import Point
 from graphene_linked_events.rest_client import LinkedEventsApiClient
 
 from palvelutarjotin.exceptions import ApiBadRequestError, ObjectDoesNotExistError
@@ -49,3 +52,26 @@ def retrieve_linked_events_data(resource, resource_id, params=None, is_staff=Fal
 def get_keyword_set_by_id(keyword_set_id):
     params = {"include": "keywords"}
     return retrieve_linked_events_data("keyword_set", keyword_set_id, params=params,)
+
+
+def bbox_for_coordinates(
+    longitude: float, latitude: float, distance: float
+) -> List[float]:
+    """
+    Calculate bounding box for the given coordinates so that the
+    bounding box corner is at the given distance (in km).
+    """
+    sw_bearing = 225
+    ne_bearing = 45
+    d = geopy_distance.distance(kilometers=distance)
+
+    starting_point = Point(latitude=latitude, longitude=longitude)
+    sw_point = d.destination(starting_point, bearing=sw_bearing)
+    ne_point = d.destination(starting_point, bearing=ne_bearing)
+
+    return [
+        sw_point.longitude,
+        sw_point.latitude,
+        ne_point.longitude,
+        ne_point.latitude,
+    ]
