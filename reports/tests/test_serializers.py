3607e32890cee0809932a2703b8ca3f2b13c73f3
Niko Lindroos <niko.j.makela@gmail.com>
2022-01-07 09:17:37 +0200

    Enrolment reports divisions information given in more detailed format
    PT-1421

--- reports/tests/test_serializers.py
+++ reports/tests/test_serializers.py
@@ -9,6 +9,18 @@ def test_enrolment_report_to_representation():
         keywords=[["kultus:5", "Musiikki"]],
         occurrence_place_position=[24.670147, 60.20042],
         study_group_unit_position=[24.862606, 60.251762],
+        study_group_unit_divisions=[
+            "ocd-division/country:fi/kunta:helsinki/osa-alue:keski-pasila",
+            "ocd-division/country:fi/kunta:helsinki/kaupunginosa:pasila",
+            "ocd-division/country:fi/kunta:helsinki/peruspiiri:pasila",
+            "ocd-division/country:fi/kunta:helsinki",
+        ],
+        occurrence_place_divisions=[
+            "ocd-division/country:fi/kunta:helsinki/osa-alue:vironniemi",
+            "ocd-division/country:fi/kunta:helsinki/kaupunginosa:kruununhaka",
+            "ocd-division/country:fi/kunta:helsinki/peruspiiri:kruununhaka",
+            "ocd-division/country:fi/kunta:helsinki",
+        ],
     )
     serializer = EnrolmentReportSerializer(instance=report)
     assert serializer.data["study_group_study_levels"][0] == {
@@ -25,6 +37,32 @@ def test_enrolment_report_to_representation():
         "longitude": 24.862606,
         "latitude": 60.251762,
     }
+    assert serializer.data["study_group_unit_divisions"] == {
+        "ocd_ids": [
+            "ocd-division/country:fi/kunta:helsinki/osa-alue:keski-pasila",
+            "ocd-division/country:fi/kunta:helsinki/kaupunginosa:pasila",
+            "ocd-division/country:fi/kunta:helsinki/peruspiiri:pasila",
+            "ocd-division/country:fi/kunta:helsinki",
+        ],
+        "country": "Fi",
+        "municipality": "Helsinki",
+        "sub_district": "Keski-pasila",
+        "neighborhood": "Pasila",
+        "district": "Pasila",
+    }
+    assert serializer.data["occurrence_place_divisions"] == {
+        "ocd_ids": [
+            "ocd-division/country:fi/kunta:helsinki/osa-alue:vironniemi",
+            "ocd-division/country:fi/kunta:helsinki/kaupunginosa:kruununhaka",
+            "ocd-division/country:fi/kunta:helsinki/peruspiiri:kruununhaka",
+            "ocd-division/country:fi/kunta:helsinki",
+        ],
+        "country": "Fi",
+        "municipality": "Helsinki",
+        "sub_district": "Vironniemi",
+        "neighborhood": "Kruununhaka",
+        "district": "Kruununhaka",
+    }
 
 
 def test_enrolment_report_to_internal():
@@ -32,6 +70,29 @@ def test_enrolment_report_to_internal():
         "study_group_study_levels": [{"id": "age_0_2", "label": "age 0-2"}],
         "occurrence_languages": [{"id": "fi", "name": "Finnish"}],
         "keywords": [{"id": "kultus:5", "name": "Musiikki"}],
+        "occurrence_place_position": {"longitude": 24.670147, "latitude": 60.20042},
+        "study_group_unit_position": {"longitude": 24.862606, "latitude": 60.251762},
+        "study_group_unit_divisions": {
+            "ocd_ids": [
+                "ocd-division/country:fi/kunta:helsinki/osa-alue:keski-pasila",
+                "ocd-division/country:fi/kunta:helsinki/kaupunginosa:pasila",
+                "ocd-division/country:fi/kunta:helsinki/peruspiiri:pasila",
+                "ocd-division/country:fi/kunta:helsinki",
+            ],
+            "country": "Fi",
+            "municipality": "Helsinki",
+            "sub_district": "Keski-pasila",
+            "neighborhood": "Pasila",
+            "district": "Pasila",
+        },
+        "occurrence_place_divisions": {
+            # no ocd_ids given in purpose
+            "country": "Fi",
+            "municipality": "Helsinki",
+            "sub_district": "Vironniemi",
+            "neighborhood": "Kruununhaka",
+            "district": "Kruununhaka",
+        },
         # Rest are for is_valid -call
         "study_group_amount_of_children": 1,
         "study_group_amount_of_adult": 1,
@@ -44,8 +105,6 @@ def test_enrolment_report_to_internal():
         "occurrence_end_time": "2021-11-14T01:00:00+02:00",
         "linked_event_id": "local-kultus:af6p5lyfxu",
         "enrolment_start_time": "2021-11-08T10:45:38.439000+02:00",
-        "occurrence_place_position": {"longitude": 24.670147, "latitude": 60.20042},
-        "study_group_unit_position": {"longitude": 24.862606, "latitude": 60.251762},
     }
     report = EnrolmentReportSerializer(data=report_json)
     report.is_valid()
@@ -55,3 +114,26 @@ def test_enrolment_report_to_internal():
     assert data["keywords"][0] == ["kultus:5", "Musiikki"]
     assert data["occurrence_place_position"] == [24.670147, 60.20042]
     assert data["study_group_unit_position"] == [24.862606, 60.251762]
+    # When ocd ids are given in Finnish, they are returned in Finnish.
+    # This is always the case (at least almost)
+    assert all(
+        e
+        in [
+            "ocd-division/country:fi/kunta:helsinki/osa-alue:keski-pasila",
+            "ocd-division/country:fi/kunta:helsinki/kaupunginosa:pasila",
+            "ocd-division/country:fi/kunta:helsinki/peruspiiri:pasila",
+            "ocd-division/country:fi/kunta:helsinki",
+        ]
+        for e in data["study_group_unit_divisions"]
+    )
+    # When ocd ids are not given, the English keys are used
+    assert all(
+        e
+        in [
+            "ocd-division/country:fi/municipality:helsinki/sub_district:vironniemi",
+            "ocd-division/country:fi/municipality:helsinki/neighborhood:kruununhaka",
+            "ocd-division/country:fi/municipality:helsinki/district:kruununhaka",
+            "ocd-division/country:fi/municipality:helsinki",
+        ]
+        for e in data["occurrence_place_divisions"]
+    )
