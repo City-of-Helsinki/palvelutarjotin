3fb1fc4eee915c3b2320865ab6fe42b95fad43b3
Tero Virtanen <tero.virtanen@mavericks.fi>
2022-07-13 13:43:03 +0300

    enable hpa on prod and stating, add load test

---
+++ k6LoadTests.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+  duration: '10m',
+  vus: 20,
+  //  vus: 1,
+  thresholds: {
+    //avg is around ?800ms? on https://palvelutarjotin-api.test.kuva.hel.ninja
+    http_req_duration: ['p(95)<5000'],
+  },
+};
+
+export default () => {
+  const url = 'https://palvelutarjotin-api.test.kuva.hel.ninja/graphql';
+  const data = 'query=query Places {places(showAllPlaces: true){data{id}}}';
+  const res = http.post(url, data, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  });
+
+  //10 loads per minute
+  sleep(6);
+};
