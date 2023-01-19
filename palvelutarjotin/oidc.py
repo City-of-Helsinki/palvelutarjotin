2e8d987d2153242a26d3b01e083923d1f19e39a8
Niko Lindroos <niko.j.makela@gmail.com>
2023-01-19 13:58:46 +0200

    Save user's last activity if the configured interval has passed
    PT-1620.
    Write the authentication timestamp to user's last_login -field on every 60 minutes.
    fix

--- palvelutarjotin/oidc.py
+++ palvelutarjotin/oidc.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.contrib.auth.models import update_last_login
+from django.utils import timezone
 from helusers.oidc import ApiTokenAuthentication
 
 
@@ -19,7 +22,31 @@ class FixTunnistamoMixin:
         super().validate_claims(id_token)
 
 
-class GraphQLApiTokenAuthentication(FixTunnistamoMixin, ApiTokenAuthentication):
+class FixLastLoginMixin:
+    def update_last_login_after_interval(self, user):
+        """
+        Update the user object last_login field if enough time has passed.
+        The setting UPDATE_LAST_LOGIN["UPDATE_INTERVAL_MINUTES"] determines
+        whether or not enough time has passed for update.
+        We don't want to update on every request,
+        because then there would be too much writes when there are lot of users.
+        """
+        minutes_since_last_update = None
+        if user.last_login:
+            minutes_since_last_update = (
+                timezone.now() - user.last_login
+            ).total_seconds() / 60
+        if (
+            minutes_since_last_update is None
+            or minutes_since_last_update
+            > settings.UPDATE_LAST_LOGIN["UPDATE_INTERVAL_MINUTES"]
+        ):
+            update_last_login(sender=None, user=user)
+
+
+class GraphQLApiTokenAuthentication(
+    FixTunnistamoMixin, FixLastLoginMixin, ApiTokenAuthentication
+):
     """
     Custom wrapper for the helusers.oidc.ApiTokenAuthentication backend.
     Needed to make it work with graphql_jwt.middleware.JSONWebTokenMiddleware,
@@ -34,6 +61,10 @@ class GraphQLApiTokenAuthentication(FixTunnistamoMixin, ApiTokenAuthentication):
         if not user_auth_tuple:
             return None
         user, auth = user_auth_tuple
+
+        if settings.UPDATE_LAST_LOGIN and settings.UPDATE_LAST_LOGIN["ENABLED"]:
+            self.update_last_login_after_interval(user)
+
         return user
 
 
--- palvelutarjotin/tests/test_oidc.py
+++ palvelutarjotin/tests/test_oidc.py
@@ -1,11 +1,29 @@
+import contextlib
 import datetime
 import jwt
 from authlib.jose.rfc7519.claims import JWTClaims
-from django.test import TestCase
+from django.test import override_settings, TestCase
 from django.test.client import RequestFactory
+from django.utils import timezone
+from freezegun import freeze_time
 from unittest import mock
 
+from organisations.factories import UserFactory
 from palvelutarjotin.oidc import GraphQLApiTokenAuthentication
+
+
+@contextlib.contextmanager
+def mocked_jwt_request(encoded_jwt):
+    with mock.patch(
+        "helusers.oidc.ApiTokenAuthentication.decode_jwt",
+        return_value=JWTClaims(
+            jwt.decode(encoded_jwt, options={"verify_signature": False}), None
+        ),
+    ):
+        yield RequestFactory().get(
+            "/fake-path",
+            HTTP_AUTHORIZATION=f"Bearer {encoded_jwt}",
+        )
 
 
 class TestOIDC(TestCase):
@@ -56,16 +74,66 @@ class TestOIDC(TestCase):
         self.assertTrue(mock_super.called)
         self.assertEqual(id_token["amr"], ["github"])
 
+    @override_settings(
+        UPDATE_LAST_LOGIN={
+            "ENABLED": True,
+            "UPDATE_INTERVAL_MINUTES": 60,
+        }
+    )
     def test_authenticate(self):
-        with mock.patch(
-            "helusers.oidc.ApiTokenAuthentication.decode_jwt",
-            return_value=JWTClaims(
-                jwt.decode(self.encoded_jwt, options={"verify_signature": False}), None
-            ),
-        ):
-            request = RequestFactory().get(
-                "/fake-path",
-                HTTP_AUTHORIZATION="Bearer {}".format(self.encoded_jwt),
-            )
+        """Successful authentication and last_login is set"""
+        with mocked_jwt_request(self.encoded_jwt) as request:
             user = self.auth_backend.authenticate(request)
             self.assertEqual(user.email, self.id_token_payload["email"])
+            self.assertIsNotNone(user.last_login)
+
+    @override_settings(
+        UPDATE_LAST_LOGIN={
+            "ENABLED": False,
+            "UPDATE_INTERVAL_MINUTES": 60,
+        }
+    )
+    def test_authenticate_with_update_last_login_disabled(self):
+        """Last login should not be set, because the feature is disabled."""
+        with mocked_jwt_request(self.encoded_jwt) as request:
+            user = self.auth_backend.authenticate(request)
+            self.assertEqual(user.email, self.id_token_payload["email"])
+            self.assertIsNone(user.last_login)
+
+    @override_settings(
+        UPDATE_LAST_LOGIN={
+            "ENABLED": True,
+            "UPDATE_INTERVAL_MINUTES": 60,
+        }
+    )
+    def test_update_last_login_before_interval(self):
+        """Last login should not be updated, because the interval has not yet passed."""
+        last_login = timezone.now() - datetime.timedelta(minutes=50)
+        user = UserFactory(last_login=last_login)
+        with mocked_jwt_request(self.encoded_jwt) as request:
+            with mock.patch(
+                "helusers.oidc.ApiTokenAuthentication.authenticate",
+                return_value=[user, None],
+            ):
+                user = self.auth_backend.authenticate(request)
+                self.assertEqual(user.last_login, last_login)
+
+    @override_settings(
+        UPDATE_LAST_LOGIN={
+            "ENABLED": True,
+            "UPDATE_INTERVAL_MINUTES": 60,
+        }
+    )
+    @freeze_time("2023-01-18 12:00:00")
+    def test_update_last_login_after_interval(self):
+        """Last login should be updated, because the interval has passed."""
+        login_time = timezone.now()
+        last_login = timezone.now() - datetime.timedelta(minutes=70)
+        user = UserFactory(last_login=last_login)
+        with mocked_jwt_request(self.encoded_jwt) as request:
+            with mock.patch(
+                "helusers.oidc.ApiTokenAuthentication.authenticate",
+                return_value=[user, None],
+            ):
+                user = self.auth_backend.authenticate(request)
+                self.assertEqual(user.last_login, login_time)
