66768c260904d445751865bd724c6abefd7ceb73
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-02-15 07:39:01 +0200

    K6 loadtests url variable

--- k6LoadTests.js
+++ k6LoadTests.js
@@ -7,13 +7,17 @@ export const options = {
   vus: 20,
   //  vus: 1,
   thresholds: {
-    //avg is around ?800ms? on https://palvelutarjotin-api.test.kuva.hel.ninja
+    //avg is around ?800ms? on https://kultus.api.stage.hel.ninja
     http_req_duration: ['p(95)<5000'],
   },
 };
 
 export default () => {
-  const url = 'https://palvelutarjotin-api.test.kuva.hel.ninja/graphql';
+  let url = 'https://kultus.api.stage.hel.ninja/graphql';
+  if (`${__ENV.K6_LOADTEST_ENV_URL}` != 'undefined') {
+    url = `${__ENV.K6_LOADTEST_ENV_URL}`;
+  }
+
   const data = 'query=query Organisations {organisations {edges {node {id } } } }';
   const res = http.post(url, data, {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
