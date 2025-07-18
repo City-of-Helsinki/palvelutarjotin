75b150d3e9c1d2316318eebfc9c766db4f5203a4
Mika Hietanen <m.hietanen84@gmail.com>
2025-07-18 08:10:45 +0300

    refactor: update CORS variable naming
    Refs: PT-1940

--- palvelutarjotin/tests/test_cors.py
+++ palvelutarjotin/tests/test_cors.py
@@ -7,7 +7,7 @@ def test_cors_allowed_origin(settings, live_server, client: Client):
     settings.CORS_ALLOWED_ORIGINS = [
         live_server.url,
     ]
-    settings.CORS_ORIGIN_ALLOW_ALL = False
+    settings.CORS_ALLOW_ALL_ORIGINS = False
     response = client.get("/admin/login/", HTTP_ORIGIN=live_server.url)
     assert response.status_code == 200
     assert response["Access-Control-Allow-Origin"] == live_server.url
@@ -16,7 +16,7 @@ def test_cors_allowed_origin(settings, live_server, client: Client):
 @pytest.mark.django_db(transaction=True)
 def test_cors_disallowed_origin(settings, live_server, client: Client):
     settings.CORS_ALLOWED_ORIGINS = [live_server.url]
-    settings.CORS_ORIGIN_ALLOW_ALL = False
+    settings.CORS_ALLOW_ALL_ORIGINS = False
     response = client.get("/admin/login/", HTTP_ORIGIN="http://malicious.com")
     assert response.status_code == 200  # The request still succeeds...
     assert (
