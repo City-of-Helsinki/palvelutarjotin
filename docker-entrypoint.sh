953e061e859b7a00e401eda84bd07d1817f53575
Jori Lindell <jori.lindell@gmail.com>
2026-08-17 16:12:05 +0300

    fix: use bash [[ ]] for SKIP_DATABASE_CHECK condition
    Use Bash-native conditional syntax in docker-entrypoint.sh to satisfy
    Sonar rule S7688.
    
    Refs: PT-2059

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -z "$SKIP_DATABASE_CHECK" ] || [ "$SKIP_DATABASE_CHECK" = "0" ]; then
+if [[ -z "$SKIP_DATABASE_CHECK" || "$SKIP_DATABASE_CHECK" = "0" ]]; then
   until nc -z -v -w30 "$DATABASE_HOST" 5432
   do
     echo "Waiting for postgres database connection..."
